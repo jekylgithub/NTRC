2c2362947d2a48fbb7e7f04a682d8102ed630c0f
jekylgithub <43798765+jekylgithub@users.noreply.github.com>
2026-07-24 01:41:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const V="ntrc-v1";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(x=>x.put(e.request,c).catch(()=>{}));return r;})
+    .catch(()=>caches.match(e.request).then(m=>m||caches.match("./index.html"))));
+});
